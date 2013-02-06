1294bee8dd0b8885ddffdf3f601aea5aa8385bf1
Raul Gallegos Hidalgo <raul.ogh@gmail.com>
2013-02-06 09:05:02 -0500

    URL Validation function and unit tests
    -The url_validator function added to functions.py (shortener).
    -Unit tests for the url_validator function added (shortener).

--- shortener/functions.py
+++ shortener/functions.py
@@ -1,11 +1,19 @@
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+from urlparse import urlparse
 
 def validate_url(_url):
-	
 	validate = URLValidator(verify_exists=False)
+	parsed = urlparse(_url)
+	if not parsed.scheme:
+		_url = 'http://' + _url
+
 	try:
 		validate(_url)
 		return True
 	except ValidationError, e:
-	    return False
+		return False
+
+
+def encode_url(_url):
+	pass
--- shortener/tests.py
+++ shortener/tests.py
@@ -8,6 +8,7 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from django.test.client import Client
 from models import master
+from functions import validate_url
 
 class ViewsTest(TestCase):
 	def test_home(self):
@@ -15,11 +16,40 @@ class ViewsTest(TestCase):
 		response = client.get('/')
 		self.assertEqual(response.status_code,200)
 
+class FunctionsTest(TestCase):
+    def test_validate_url(self):
+        self.assertEqual(validate_url('http://url.com'),True)
+        self.assertEqual(validate_url('http://www.url.com'),True)
+        self.assertEqual(validate_url('http://url.'),False)
+        self.assertEqual(validate_url('http://url.url.com'),True)
+        self.assertEqual(validate_url('http://sub1.sub2.url.com'),True)
+        self.assertEqual(validate_url('http://url.com/path/to/some/folder'),True)
+        self.assertEqual(validate_url('http://url.com/path/to/some/folder/'),True)
+        self.assertEqual(validate_url('http://url.com/?param1'),True)
+        self.assertEqual(validate_url('http://url.com/?param1=1'),True)
+        self.assertEqual(validate_url('http://url.com/?param1=something'),True)
+        self.assertEqual(validate_url('https://newprotocol.com/'),True)
+        self.assertEqual(validate_url('ftp://newprotocol.com/'),True)
+        self.assertEqual(validate_url('https://www.google.com.pe/search?q=url+validation+examples&oq=url+validation+examples&aqs=chrome.0.57j61j0j60l3.3437&sourceid=chrome&ie=UTF-8'),True)
+        
+        # Tests without protocol ...
+        self.assertEqual(validate_url('url.com'),True)
+        self.assertEqual(validate_url('www.url.com'),True)
+        self.assertEqual(validate_url('url.'),False)
+        self.assertEqual(validate_url('url.url.com'),True)
+        self.assertEqual(validate_url('sub1.sub2.url.com'),True)
+        self.assertEqual(validate_url('url.com/path/to/some/folder'),True)
+        self.assertEqual(validate_url('url.com/path/to/some/folder/'),True)
+        self.assertEqual(validate_url('url.com/?param1'),True)
+        self.assertEqual(validate_url('url.com/?param1=1'),True)
+        self.assertEqual(validate_url('url.com/?param1=something'),True)
+        self.assertEqual(validate_url('newprotocol.com/'),True)
+        self.assertEqual(validate_url('ftp:/newprotocol.com/'),False)
+        self.assertEqual(validate_url('https:/www.google.com.pe/search?q=url+validation+examples&oq=url+validation+examples&aqs=chrome.0.57j61j0j60l3.3437&sourceid=chrome&ie=UTF-8'),False)
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
-        print 'hola'
         self.assertEqual(1 + 1, 2)
