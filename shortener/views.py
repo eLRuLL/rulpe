2f31690d0fb42a07fa898121e73bd7f1bfcc5ec8
Raul Gallegos Hidalgo <raul.ogh@gmail.com>
2013-02-10 21:44:19 -0500

    URL Validation bug solved
    - The "url_validation_error.html" was given a "None" value in the html
    rendered because of the response of "functions.validate_url". Now it
    stores temporally the previous "real_url" variable to show as an error
    message.
    
    - 404.html template added. (only working for debug=False)*.
    
    *beware of the static files.

--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response, redirect, render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import redirect, render
 from random import randint
 from models import master
 from datetime import datetime
@@ -20,11 +20,11 @@ def home(request):
 
 
 def shortener(request):
-	long_url = request.POST.get('long_url','')
+	real_url = request.POST.get('long_url','')
 
-	long_url = validate_url(long_url)
+	long_url = validate_url(real_url)
 	if not long_url:
-		return render(request,'url_validation_error.html',{'bad_url' : long_url})
+		return render(request,'url_validation_error.html',{'bad_url' : real_url})
 
 	elif master.objects.filter(long_url=long_url).exists():
 		temp_short = master.objects.get(long_url=long_url)
@@ -43,8 +43,10 @@ def shortener(request):
 		return render(request,'shortener.html',{'long_url':long_url,'short_url':short_url})
 
 def counter(request,shorten):
-
-	temp_short = master.objects.get(id=decode(shorten))
+	try:
+		temp_short = master.objects.get(id=decode(shorten))
+	except:
+		raise Http404()
 	temp_short.clicks += 1
 	temp_short.save()
 	return redirect(temp_short.long_url)
